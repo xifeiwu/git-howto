45452d804d369b54e676f79a42a750be61803f51
wuxifei <xfwu@conviva.com>
2022-07-20 18:53:48 +0800

    add more demo

---
+++ 1-storage/1-list-objects.ts
@@ -0,0 +1,7 @@
+
+import { REPO_DIR, createRepoIfNotExist } from './create-repo';
+import { listObjects } from './objects';
+
+// createRepoIfNotExist();
+const files = listObjects(REPO_DIR);
+console.log(files);
