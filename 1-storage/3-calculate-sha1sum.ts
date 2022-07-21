e02344a1f0f2d8a1f75db5a2368d315ad41dd2b3
wuxifei <xfwu@conviva.com>
2022-07-22 00:00:46 +0800

    add 3-calculate-sha1sum.ts

---
+++ 1-storage/3-calculate-sha1sum.ts
@@ -0,0 +1,15 @@
+
+import { execSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+import { calSha1Sum } from './objects';
+
+// 1-list-objects.ts
+// const sha1sum = calSha1Sum('blob', fs.readFileSync(path.join(__dirname, '2-check-sha.ts')));
+// console.log(sha1sum);
+
+const file = '/Users/wuxifei/Documents/projects/conviva/semantic-mapper-frontend/src/pages/components/RuleMapper/index.tsx'
+const sha1sum = calSha1Sum('blob', fs.readFileSync(file));
+console.log(sha1sum);
+// const tagSha1sum = calSha1Sum('tag', execSync(`git cat-file -p v1.0`));
+// console.log(tagSha1sum);
--- 1-storage/objects.ts
+++ 1-storage/objects.ts
@@ -1,8 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as zlib from 'zlib';
-import {readDirRecursive} from '../utils/common';
-import { REPO_DIR, createRepoIfNotExist } from './create-repo';
+import {readDirRecursive, sha1sum} from '../utils/common';
 
 function unzip(path: string) {
   if (!fs.existsSync(path)) {
@@ -12,25 +11,60 @@ function unzip(path: string) {
   return zlib.unzipSync(buffer).toString('utf-8');
 }
 
-createRepoIfNotExist();
-process.chdir(REPO_DIR);
-const OBJECTS_DIR = `${REPO_DIR}/.git/objects`;
-const files: {
-  path: string;
-  shasum: string;
-  content: string;
-}[] = readDirRecursive(OBJECTS_DIR).map(it => {
-  const shasum = it.split('/').join('');
-  return {
-    path: it,
-    shasum,
-    content: unzip(path.join(OBJECTS_DIR, it))
+export function listObjects(repoDir: string) {
+  if (!fs.existsSync(repoDir)) {
+    throw new Error(`repoDir ${repoDir} not exist`);
   }
-});
-// const buffer = fs.readFileSync();
-// console.log(buffer);
-// console.log(unzip(buffer).toString());
-// const shaList = files.map(it => {
-  // const 
-// });
-console.log(files);
+  process.chdir(repoDir);
+  const OBJECTS_DIR = `${repoDir}/.git/objects`;
+  const files: {
+    path: string;
+    shasum: string;
+    content: string;
+  }[] = readDirRecursive(OBJECTS_DIR, {
+    dirFilter(dirPath) {
+      if (!dirPath) {
+        return false;
+      }
+      const lastDir = dirPath.split('/').pop();
+      if (!lastDir) {
+        return false;
+      }
+      return lastDir.length === 2;
+    }
+  }).map(it => {
+    const shasum = it.split('/').join('');
+    return {
+      path: it,
+      shasum,
+      content: unzip(path.join(OBJECTS_DIR, it))
+    }
+  });
+  return files;
+}
+
+export function checkObject(repoDir: string, shasum: string) {
+  if (!fs.existsSync(repoDir)) {
+    throw new Error(`repoDir ${repoDir} not exist`);
+  }
+  process.chdir(repoDir);
+  const OBJECTS_DIR = `${repoDir}/.git/objects`;
+  const shasumPath = shasum.slice(0, 2) + '/' + shasum.slice(2);
+  const filePath = path.join(OBJECTS_DIR, shasumPath);
+  const buffer = zlib.unzipSync(fs.readFileSync(filePath));
+  const str = buffer.toString('utf-8');
+  const newShaSum = sha1sum(buffer);
+  // const content = 
+  console.log(shasum);
+  console.log(buffer);
+  console.log(str);
+  console.log(newShaSum);
+}
+
+export function calSha1Sum(type: 'blob' | 'tree' | 'commit' | 'tag', content: string | Buffer) {
+  if (!Buffer.isBuffer(content)) {
+    content = Buffer.from(content);
+  }
+  const prefix = Buffer.from(`${type} ${content.byteLength}\x00`);
+  return sha1sum(Buffer.concat([prefix, content]));
+}
