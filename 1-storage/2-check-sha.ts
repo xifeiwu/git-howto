8cbcee26e5219d42d70b81b498e7075245830199
wuxifei <xfwu@conviva.com>
2022-07-28 15:23:17 +0800

    update demo code

--- 1-storage/2-check-sha.ts
+++ 1-storage/2-check-sha.ts
@@ -3,4 +3,4 @@ import { checkObject } from './objects';
 
 // 713c888e2a2a4def616a3f074426ee0845b2721f
 // checkObject(REPO_DIR, 'e59d015549d324f4522761b30a1c40c0bbed6d97');
-checkObject(REPO_DIR, 'bae919926b49fcaf220b1fb46996cfb3213f61da');
+checkObject(REPO_DIR, 'f227bc1a768a97bfd55890a582238906233c40d1');
--- 1-storage/3-calculate-sha1sum.ts
+++ 1-storage/3-calculate-sha1sum.ts
@@ -2,6 +2,7 @@
 import { execSync } from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
+import { REPO_DIR } from './create-repo';
 import { calSha1Sum } from './objects';
 
 // 1-list-objects.ts
@@ -9,7 +10,8 @@ import { calSha1Sum } from './objects';
 // console.log(sha1sum);
 
 // const file = '/Users/wuxifei/Documents/projects/conviva/semantic-mapper-frontend/src/pages/components/RuleMapper/index.tsx'
-const file = '/Users/xifeiwu/Documents/projects/bash/project/git-howto/1-storage/git-repo/big-file.txt';
+// const file = '/Users/xifeiwu/Documents/projects/bash/project/git-howto/1-storage/git-repo/big-file.txt';
+const file = path.resolve(REPO_DIR, 'file2.txt');
 const sha1sum = calSha1Sum('blob', fs.readFileSync(file));
 console.log(sha1sum);
 // const tagSha1sum = calSha1Sum('tag', execSync(`git cat-file -p v1.0`));
