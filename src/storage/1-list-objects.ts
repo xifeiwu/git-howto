431dfc39acea29598e1dcd1adad738ff09a5ae88
wuxifei <xfwu@conviva.com>
2022-09-02 10:24:09 +0800

    remove dir storage

--- src/storage/1-list-objects.ts
+++
@@ -1,7 +0,0 @@
-
-import { REPO_DIR, createRepoIfNotExist } from './create-repo';
-import { listObjects } from './objects';
-
-createRepoIfNotExist();
-const files = listObjects(REPO_DIR);
-console.log(files);
--- src/storage/2-check-sha.ts
+++
@@ -1,6 +0,0 @@
-import { REPO_DIR, createRepoIfNotExist } from './create-repo';
-import { checkObject } from './objects';
-
-// 713c888e2a2a4def616a3f074426ee0845b2721f
-// checkObject(REPO_DIR, 'e59d015549d324f4522761b30a1c40c0bbed6d97');
-checkObject(REPO_DIR, 'f227bc1a768a97bfd55890a582238906233c40d1');
